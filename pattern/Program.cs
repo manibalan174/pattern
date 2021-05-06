990cd96f3da8db9323d9309292b06040d7566e26
manibalan174 <45997825+manibalan174@users.noreply.github.com>
2021-05-06 13:18:37 +0530

    c# pattern file uploded

---
+++ pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (int row = 8; row >= 1; --row)
+            {
+            for (int col = 1; col <= row; ++col)
+            {
+               System.Console.Write("*");
+            }
+            
+            System.Console.WriteLine();
+            }
+        //     Console.WriteLine("Hello World!");
+        //    string Age = Console.ReadLine();
+           //int Ages= 1 + Age;
+            //Console.WriteLine(Age);
+            
+
+        }
+    }
+}
